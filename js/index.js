fb594d9f3e417884afadd5a010c2b04b17d48392
Oupsy <max.poette@gmail.com>
2020-09-19 13:05:10 +0200

    code clean

--- js/index.js
+++ js/index.js
@@ -2,19 +2,11 @@
  * Signatures
  * @author ADAPEI de la Meuse
  */
-
- // Version
 const RELEASE = "2.0.1"
 console.log("Signatures v " + RELEASE);
 document.getElementById("appRelease").innerText = RELEASE;
 
-/**
- * Déclaration des différents items du formulaire
- * Pour ajouter un item il faut ajouter un champ de saisie au formulaire HTML
- * /!\ Ce champ de saisie doit avoir un ID de la forme : "#itemInput" 
- * Il faut aussi ajouter un champ cible dans lequel sera affiché la valeur de ce dernier
- * Celui doit aussi être de la forme "#itemOutput"
- */
+// items présents dans le formulaire (item + "Input" / item + "Output")
 const ITEMS = [
   "name",
   "orgTitle",
@@ -26,133 +18,96 @@ const ITEMS = [
   "pole"
 ];
 
-/**
- * Définition des couleurs
- */
-const COLORS = {
+const SEPARATOR_COLORS = {
   ADAPEI: "#e6b000",
   DIOCESE: "#0b4a6d"
 };
 
-/**
- * Le dossier par défaut d'une image
- */
-const IMG_PATH = "img/logos/";
+const DEFAULT_IMG_PATH = "img/logos/";
 
 /**
- * Pour ajouter un pole il suffit de le définir ici et d'ajouter son logo dans le dossier img/logos
  * hidden -  cache(true) le pole de la liste
  * domaine - permet de selectionner automatiquement le pole en fonction du domaine que l'utilisateur aura rentré dans le champ
  * color - permet de redéfinir la couleur du séparateur de la signature
+ * src - DEFAULT_IMG_PATH + src -> Source du logo à afficher
  */
-const LIST_POLES = [
-  adapei = { lib: "Adapei de la Meuse", color : COLORS.ADAPEI, src:  "adapei.png"},
-  retroActif = { lib: "Les rétro actifs", color : COLORS.ADAPEI, src:  "retroActif.png"},
-  meuseInsertion = { lib: "Meuse Insertion", color : COLORS.ADAPEI, src: "meuseInsertion.png"},
-  adultesDependants = { lib: "Pôle Adultes Dépendants", color : COLORS.ADAPEI, src:  "adultesDependants.png"},
-  agricole = { lib: "Pôle Agricole", color : COLORS.ADAPEI, src:  "agricole.png"},
-  enfance = { lib: "Pôle Enfance", color : COLORS.ADAPEI, src: "enfance.png"},
-  habitat = { lib: "Pôle Habitat", color : COLORS.ADAPEI, src:  "habitat.png"},
-  industriel = { lib: "Pôle Industriel", color : COLORS.ADAPEI, src: "industriel.png"},
-  mobiMeuse = { lib: "Mobi'Meuse", color: COLORS.ADAPEI, src: "mobimeuse.png" },
-  diocese = { lib: "Diocèse", color : COLORS.DIOCESE, src:  "diocese.png", hidden: true, domaine: "@catholique55.fr"},
-  mda = { lib: "Maison des Adolescents", color : COLORS.DIOCESE, src:  "mda.jpg", hidden: true, domaine: "@mda55.fr"},
+const POLES_ARRAY = [
+  adapei = { lib: "Adapei de la Meuse", color : SEPARATOR_COLORS.ADAPEI, src:  "adapei.png"},
+  retroActif = { lib: "Les rétro actifs", color : SEPARATOR_COLORS.ADAPEI, src:  "retroActif.png"},
+  meuseInsertion = { lib: "Meuse Insertion", color : SEPARATOR_COLORS.ADAPEI, src: "meuseInsertion.png"},
+  adultesDependants = { lib: "Pôle Adultes Dépendants", color : SEPARATOR_COLORS.ADAPEI, src:  "adultesDependants.png"},
+  agricole = { lib: "Pôle Agricole", color : SEPARATOR_COLORS.ADAPEI, src:  "agricole.png"},
+  enfance = { lib: "Pôle Enfance", color : SEPARATOR_COLORS.ADAPEI, src: "enfance.png"},
+  habitat = { lib: "Pôle Habitat", color : SEPARATOR_COLORS.ADAPEI, src:  "habitat.png"},
+  industriel = { lib: "Pôle Industriel", color : SEPARATOR_COLORS.ADAPEI, src: "industriel.png"},
+  mobiMeuse = { lib: "Mobi'Meuse", color: SEPARATOR_COLORS.ADAPEI, src: "mobimeuse.png" },
+  diocese = { lib: "Diocèse", color : SEPARATOR_COLORS.DIOCESE, src:  "diocese.png", hidden: true, domaine: "@catholique55.fr"},
+  mda = { lib: "Maison des Adolescents", color : SEPARATOR_COLORS.DIOCESE, src:  "mda.jpg", hidden: true, domaine: "@mda55.fr"},
 ];
 
-
-// Utilisés pour la selection de l'icone.
-const poleInput = document.getElementById("poleInput");
-
-/**
- * Remplissage du poleInput grâce au tableau LIST_POLES
- */
-LIST_POLES.forEach((pole, i) => {
-  let option = addOption(poleInput, i, pole.lib);
-  if(pole.hidden){
-    hideOption(option);
-  }
-});
-
+// Utilisé pour la selection de l'icone.
+var poleInput = document.getElementById("poleInput");
 
 // Désactivation de l'envoi par défaut du formulaire
 document.getElementById("inputForm").addEventListener("submit", (e) => e.preventDefault());
 
-/**
- * Events binding
- */
+// Remplissage du Select
+POLES_ARRAY.forEach((pole, index) => {
+  let option = addOptionToSelect(poleInput, index, pole.lib);
+  if(pole.hidden) {
+    hideOption(option);
+  }
+});
+
+// Binding d'evenement "change" sur les elements du tableau
 ITEMS.forEach(item => {
   // Le champ de saisie
   let input = item + "Input";
   // La cible pour l'affichage
   let output = item + "Output";
+
   document.getElementById(input).addEventListener("change", (e) =>
-    mirror(e.target, document.getElementById(output))
+    displayInputToOutput(e.target, document.getElementById(output))
   );
 });
 
-
 /**
- * Permet de définir si la cible est une image ou par défaut : un champ texte
+ * Affichage de l'input dans la cible (Image ou texte)
  * @param {HTMLFormElement} input 
  * @param {HTMLElement} output 
  */
-function mirror(input, output){
+function displayInputToOutput(input, output){
   // l'input est vide
   if(!input.value) return;
-  
+
   // l'output est une image
   if(output.nodeName === "IMG"){
-    mirrorImg(input, output);
+    output.src = DEFAULT_IMG_PATH + POLES_ARRAY[input.value].src;
+    setSeparatorBorderColor(document.getElementById("separator"), POLES_ARRAY[input.value].color);
   } else {
-    // l'output est du texte
-    mirrorInput(input, output);
-
-    // le champ modifié est le domaine, on check si le domaine est déclencheur
-    // (utilisé pour les Pôles cachés)
+    output.innerText = input.value;
     if(input.id === "domainInput"){
-      domainChanged(input);
+      selectPoleFromDomaine(input);
     }
   }
 }
 
 /**
- * Reflete la valeur d'un champ de formulaire dans un element
- *
- * @param {HTMLFormElement} input
- * @param {HTMLElement} output
+ * Défini la propriété bordercolor sur l'élément separator
+ * @param {HTMLDivElement} separator Séparateur logo -> texte
+ * @param {String} color code couleur à définir
  */
-function mirrorInput(input, output) {
-  output.innerText = input.value;
+function setSeparatorBorderColor(separator, color){
+  separator.style.borderColor = color;
 }
 
 /**
- * Indique la source de l'image à afficher
- * 
- * @param {HTMLFormElement} input SELECT
- * @param {HTMLElement} output IMG
- */
-function mirrorImg(input, output) {
-  // définition de la source du logo
-  output.src = IMG_PATH + LIST_POLES[input.value].src;
-  // attribution de la couleur du séparator (@SEE COLORS)
-  document
-    .getElementById("separator")
-    .style
-    .borderColor = LIST_POLES[input.value].color;
-}
-
-function setSeparatorColor(){
-
-}
-
-/**
- * Si le domaine est changé -> Check dans la liste des poles si
- * une entrée correspond.
+ * Si le domaine est changé -> Check dans le tableau des poles si une entrée correspond.
  * Si c'est le cas -> on change la value du select et on trigger l'evenement change
  * @param {HTMLFormElement} input 
  */
-function domainChanged(input){
-  LIST_POLES.forEach((pole, i) => {
+function selectPoleFromDomaine(input){
+  POLES_ARRAY.forEach((pole, i) => {
     if(input.value === pole.domaine){
       selectValue(poleInput, i);
     }
@@ -161,11 +116,11 @@ function domainChanged(input){
 
 /**
  * Ajoute une option (valeur + texte) au select
- * @param {HTMLSelectElement} select target
+ * @param {HTMLSelectElement} select selectElement dans le quel on doit ajouter l'option
  * @param {String} optionValue 
  * @param {String} optionText 
  */
-function addOption(select, optionValue, optionText){
+function addOptionToSelect(select, optionValue, optionText){
   let option = document.createElement('option');
   option.appendChild(document.createTextNode(optionText));
   option.value = optionValue;
